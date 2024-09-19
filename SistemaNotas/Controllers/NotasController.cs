b3c8d6c3676b390ba10a2155ab9715c8a3bb1826
Igor Lima <62576492+iguulima@users.noreply.github.com>
2024-09-18 23:48:54 -0300

    codigo

---
+++ SistemaNotas/Controllers/NotasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaNotas.Models;
+
+namespace SistemaNotas.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calcular(NotasModel notas)
+        {
+            ModelState.Clear();
+
+            if (validar(notas)) notas.CalcularMedia();
+
+            return View("Index", notas);
+        }
+
+        private bool validar(NotasModel nota)
+        {
+            bool valid = true;
+            if (testeErro(nota.Nota1, "Nota1")) valid = false;
+            if (testeErro(nota.Nota2, "Nota2")) valid = false;
+            return valid;
+        }
+
+        private bool testeErro(float? nota, string name)
+        {
+            bool valid = false;
+            if (nota is null)
+            {
+                ModelState.AddModelError(name, "Insira uma nota válida");
+                valid = true;
+            }
+            
+            if (nota < 0 || nota > 10)
+            {
+                ModelState.AddModelError(name, "O valor deve estar compreendido entre 0 e 10");
+                valid = true;
+            }
+            return valid;
+        }
+    }
+}
---
+++ SistemaNotas/Models/NotasModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaNotas.Models
+{
+    public class NotasModel
+    {
+        public float? Nota1 { get; set; }
+        public float? Nota2 { get; set; }
+        public float? Media { get; set; }
+
+        public void CalcularMedia()
+        {
+            Media = (Nota1.Value + Nota2.Value) / 2;
+        }
+    }
+}
